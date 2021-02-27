e39136b3c1e2f35c8afe2b7c30a4de5949f5bb2a
acarkh <acar.khm@gmail.com>
2021-02-27 23:43:21 +0300

    aynı hatayı tekrarlamamak için

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = ({
-  token: "ODE0OTMwNjM3NDY2NjMyMzAy.YDlBTA.JCPinfhnnV_Vgz_6PUQGozKf4Z0",
-  prefix: "e?",
-  mongoURL: "mongodb://localhost:27017",
-  githubRepository: "acarkh/elisly",
+  token: "",
+  prefix: "",
+  mongoURL: "",
+  githubRepository: "",
   color: 0x00FFFF,
   supportServer: ""
 });
