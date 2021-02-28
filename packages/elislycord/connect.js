2c374b759f2100b347e7fb999aa1e3ae3b624d01
acarkh <acar.khm@gmail.com>
2021-02-28 21:45:07 +0300

    gecikme süresi hesaplaması tamamlandı. (elislycord)

--- packages/elislycord/connect.js
+++ packages/elislycord/connect.js
@@ -6,29 +6,13 @@ module.exports = (async(options, callback) => {
   const zlib = require("fast-zlib");
 
   const ws = new WebSocket("wss://gateway.discord.gg/?v=6&encoding=json&compress=zlib-stream");
+
   const inflator = new zlib.Inflate({
     "chunkSize": 65535
   });
 
-  const intents = ({
-    "GUILDS": 1 << 0,
-    "GUILD_MEMBERS": 1 << 1,
-    "GUILD_BANS": 1 << 2,
-    "GUILD_EMOJIS": 1 << 3,
-    "GUILD_INTEGRATIONS": 1 << 4,
-    "GUILD_WEBHOOKS": 1 << 5,
-    "GUILD_INVITES": 1 << 6,
-    "GUILD_VOICE_STATES": 1 << 7,
-    "GUILD_PRESENCES": 1 << 8,
-    "GUILD_MESSAGES": 1 << 9,
-    "GUILD_MESSAGE_REACTIONS": 1 << 10,
-    "GUILD_MESSAGE_TYPING": 1 << 11,
-    "DIRECT_MESSAGES": 1 << 12,
-    "DIRECT_MESSAGE_REACTIONS": 1 << 13,
-    "DIRECT_MESSAGE_TYPING": 1 << 14
-  });
-
   let lastSequence = null;
+  let sentAt;
 
   ws.on("message", async(data) => {
     {
@@ -41,6 +25,8 @@ module.exports = (async(options, callback) => {
 
       data = JSON.parse(data.toString());
     }
+
+
 
     if(data.op === 10) {
       const heartbeatInterval = data.d.heartbeat_interval;
@@ -60,7 +46,7 @@ module.exports = (async(options, callback) => {
             "activities": options.activity ? [options.activity] : [],
             "status": options.status || "dnd"
           },
-          "intents": Object.values(intents).reduce((bits, next) => bits |= next, 0)
+          "intents": 32767 // all intents
         }
       }));
 
@@ -69,6 +55,8 @@ module.exports = (async(options, callback) => {
           "op": 1,
           "d": lastSequence
         }));
+
+        sentAt = Date.now();
       }, heartbeatInterval);
     } else if(data.op === 0) {
       if(data.t === "READY") {
@@ -82,10 +70,13 @@ module.exports = (async(options, callback) => {
       }
 
       await callback(data.t, data.d, client);
+    } else if(data.op === 11) {
+      client.set("ping", Date.now() - sentAt);
     }
 
     lastSequence = data.s;
   });
+
 
   ws.on("error", console.error);
   ws.on("close", console.log);
