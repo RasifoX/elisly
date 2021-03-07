866bfb18cf50afa5c3e3a97c7ebe381212e465f2
acarkh <acar.khm@gmail.com>
2021-03-07 14:24:22 +0300

    request sistemi tamamlandı.

--- packages/elislycord/README.md
+++ packages/elislycord/README.md
@@ -6,7 +6,7 @@ Elisly tarafından kullanılan Discord API ile bağlanmayı sağlayan bir paketi
 + erlpack (etf) ile şifrelemek
 + 0 cache
 + WebSocket bağlantısı
-+ API'ye istek gönderme sistemi (bakımda/kullanılabilir)
++ API'ye istek gönderme sistemi
 
 ## Lisans
 Kütüphanenin kodları [MIT lisansı](https://github.com/acarkh/elisly/blob/main/LICENSE) ile korunmaktadır. Herhangi bir kopyalanma durumunda lisans koşulları ihlal edilirse gerekli işlemler yapılacaktır.
--- packages/elislycord/src/request.js
+++ packages/elislycord/src/request.js
@@ -2,20 +2,18 @@ const FormData = require("form-data");
 const fetch = require("node-fetch");
 const privateStore = require("./privateStore.js");
 
-const queue = [];
-let interval;
-
 module.exports = (async(method, url, body = {}) => {
-  console.log(interval)
-  if(method === "GET") {
-    queue.push([`https://discord.com/api/v8${url}`, {
+  let request;
+
+  if(method === "GET" || method === "DELETE") {
+    request = [`https://discord.com/api/v8${url}`, {
       method,
       "headers": {
         "Authorization": `Bot ${privateStore.get("token")}`,
         "Content-Type": "application/json"
       }
-    }]);
-  } else if(method === "POST") {
+    }];
+  } else if(method === "POST" || method === "PATCH") {
     if(body.files) {
       const form = new FormData();
 
@@ -30,39 +28,36 @@ module.exports = (async(method, url, body = {}) => {
         form.append("payload_json", JSON.stringify(body));
       }
 
-      queue.push([`https://discord.com/api/v8${url}`, {
+      request = [`https://discord.com/api/v8${url}`, {
         method,
         "headers": {
           "Authorization": `Bot ${privateStore.get("token")}`,
           "Content-Type": form.getHeaders()["content-type"]
         },
         "body": form
-      }]);
+      }];
     } else {
-      queue.push([`https://discord.com/api/v8${url}`, {
+      request = [`https://discord.com/api/v8${url}`, {
         method,
         "headers": {
           "Authorization": `Bot ${privateStore.get("token")}`,
           "Content-Type": "application/json"
         },
         "body": JSON.stringify(body)
-      }]);
+      }];
     }
   }
 
   return new Promise((resolve) => {
-    if(!interval) {
-      interval = setInterval(async() => {
-        if(queue.length !== 0) {
-          const requestData = queue.pop();
-          const data = await fetch(...requestData).then((result) => result.json());
+    let data = await fetch(...request).then((res) => res.json());
 
-          resolve(data);
-        } else {
-          clearInterval(interval);
-          interval = undefined;
-        }
-      }, 100);
+    if(data.retry_after) {
+      setTimeout(async() => {
+        data = await fetch(...request).then((res) => res.json());
+        resolve(data);
+      }, data.retry_after);
+    } else {
+      resolve(data);
     }
   });
 });
